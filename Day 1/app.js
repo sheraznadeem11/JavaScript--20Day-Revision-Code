47b831d5cc0470d2d184de91ff442099c75c9311
Sheraz Nadeem <your_email@gmail.com>
2026-02-27 15:27:12 +0500

    comment move nex code

--- Day 1/app.js
+++ Day 1/app.js
@@ -1,16 +1,19 @@
-  let empName="Sheraz Nadeem";
-  let salary=20000;
-  let tax=0;
-  let netSalary=0;
-  if(salary > 15000){
-    tax=5/100*salary;
-  }
-  else
-  {
-    tax=10/100*salary;
-  }
-console.log("Employee Name",empName);
-console.log("Employee Salary", salary );
-console.log("Tax on Salary", tax);
-netSalary=salary-tax
-console.log("Net Salary :", netSalary);
+//   let empName="Sheraz Nadeem";
+//   let salary=20000;
+//   let tax=0;
+//   let netSalary=0;
+//   if(salary > 30000){
+//     tax=10/100*salary;
+//   }
+//   else
+//   {
+//     tax=5/100*salary;
+//   }
+// console.log("Employee Name",empName);
+// console.log("Employee Salary", salary );
+// console.log("Tax on Salary", tax);
+// netSalary=salary-tax
+// console.log("Net Salary :", netSalary);
+
+
+
