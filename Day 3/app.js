d66d0ef3098567a8bce5a2cce275179a309410fd
Sheraz Nadeem <sheraznadeem273@gmail.com>
2026-03-04 15:25:56 +0500

    code commit

--- Day 3/app.js
+++ Day 3/app.js
@@ -1,10 +1,10 @@
-for (let i=1 ; i<=10; i++ ){
-    console.log(i);
+// for (let i=1 ; i<=10; i++ ){
+//     console.log(i);
 
     
-}
+// }
 
 
-for (let i=20 ; i>=1; i-- ){
-    console.log(i);
-}
+// for (let i=20 ; i>=1; i-- ){
+//     console.log(i);
+// }
