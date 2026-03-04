0d613f75170f1f56dd38848108df1d2fb3e13c2f
Sheraz Nadeem <sheraznadeem273@gmail.com>
2026-03-04 15:30:30 +0500

    for loop sum

--- Day 3/app.js
+++ Day 3/app.js
@@ -8,3 +8,13 @@
 // for (let i=20 ; i>=1; i-- ){
 //     console.log(i);
 // }
+
+
+// let i;
+// let sum=0;
+// for(i=1; i<=5; i++){
+//     console.log(i);
+//     sum=sum+i
+// }
+// console.log("=============");
+// console.log(sum)
