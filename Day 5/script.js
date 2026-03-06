72cfe25b8c452837c8ed4632afc9776088f35ff6
Sheraz Nadeem <sheraznadeem273@gmail.com>
2026-03-06 17:20:13 +0500

    For in loop Data Print

--- Day 5/script.js
+++ Day 5/script.js
@@ -1,9 +1,19 @@
 //For of For in Concept//
 
-let empName=prompt("Enter Name");
-let strsize=0;
-for(let x of empName){
-    console.log("letter",x);
-    strsize++;
+// let empName=prompt("Enter Name");
+// let strsize=0;
+// for(let x of empName){
+//     console.log("letter",x);
+//     strsize++;
+// }
+// console.log("Letter  Length",strsize);
+
+let employ={
+    fullName:"Sheraz Nadeem",
+    age:20,
+    JobStatus:"Developer",
+    salary:200000,
 }
-console.log("Letter  Length",strsize);
+for(let emp in employ){
+    console.log(emp,":",employ[emp]);
+}
