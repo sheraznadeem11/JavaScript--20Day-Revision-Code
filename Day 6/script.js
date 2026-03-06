6252e4bdf57dea9d7826f6f1b606341ae3b57132
Sheraz Nadeem <sheraznadeem273@gmail.com>
2026-03-06 18:02:19 +0500

    make chanes in this program and odd number i success fully run

--- Day 6/script.js
+++ Day 6/script.js
@@ -2,7 +2,9 @@
 //     console.log(i);
 // }
 
-for (let i=2; i<=100; i+=2)
-{
-    console.log(i);
+// for (let i=2; i<=100; i+=2){
+//     console.log(i)
+// }
+for (let i=1; i<=100; i+=2){
+    console.log(i)
 }
