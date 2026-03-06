3810d75d2b79465ca3053c3bf774c9178c564d6f
Sheraz Nadeem <sheraznadeem273@gmail.com>
2026-03-06 17:16:03 +0500

    For of Loop

---
+++ Day 5/script.js
@@ -0,0 +1,9 @@
+//For of For in Concept//
+
+let empName=prompt("Enter Name");
+let strsize=0;
+for(let x of empName){
+    console.log("letter",x);
+    strsize++;
+}
+console.log("Letter  Length",strsize);
