82429a40b18bfac535a6f5bf6df396f6e415201e
Sheraz Nadeem <sheraznadeem273@gmail.com>
2026-03-08 16:35:12 +0500

    Horizontal tab \t

--- Day 9/app.js
+++ Day 9/app.js
@@ -1,2 +1,8 @@
-let data ="How are you \nI am fine"
+//NEW LINE//
+// let data ="How are you \nI am fine"
+// console.log(data);
+
+
+//Horizantal tab
+let data ="Your name : Sheraz\tNadeem"
 console.log(data);
