58cd6b8ee6f297b8379993bbb6ae24605bf31208
Sheraz Nadeem <sheraznadeem273@gmail.com>
2026-03-12 10:04:48 +0500

    use arry methor arry value print all and value sum total prin use for of loop

--- Day 12/script.js
+++ Day 12/script.js
@@ -1,5 +1,17 @@
-let arry=["Bilal","ahmad","iqram"];
-let add=arry.push("ali");
-let add=arry.pop();
-let add=arry.length();
-console.log(add,arry);
+// let arry=["Bilal","ahmad","iqram"];
+// let add=arry.push("ali");
+// let add=arry.pop();
+// let add=arry.length();
+// console.log(add,arry);
+
+
+//assignment//
+
+let arry=[2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+let sum=0;
+for (let x of arry){
+    console.log(x);
+    sum+=x
+
+}
+console.log("sum of all values",sum);
