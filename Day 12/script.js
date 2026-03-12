07b33a04637916b7e5f96a654bdf3e8d30fc07c7
Sheraz Nadeem <sheraznadeem273@gmail.com>
2026-03-12 14:38:37 +0500

    reverce print arry data nubmer assignment 2

--- Day 12/script.js
+++ Day 12/script.js
@@ -7,11 +7,17 @@
 
 //assignment//
 
-let arry=[2,3,4,5,6,7,8,9,10,11,12,13,14,15];
-let sum=0;
-for (let x of arry){
-    console.log(x);
-    sum+=x
+// let arry=[2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+// let sum=0;
+// for (let x of arry){
+//     console.log(x);
+//     sum+=x
 
+// }
+// console.log("sum of all values",sum);
+
+//assignment number 2//
+let no=[2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+for(let i=no.length; i>0 ; i--){
+    console.log(no[i]);
 }
-console.log("sum of all values",sum);
