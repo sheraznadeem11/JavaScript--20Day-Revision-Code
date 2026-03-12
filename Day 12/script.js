0d8850025fead1f3103a5f4798d690578cab0026
Sheraz Nadeem <sheraznadeem273@gmail.com>
2026-03-12 14:43:15 +0500

    arry larg number print

--- Day 12/script.js
+++ Day 12/script.js
@@ -17,7 +17,20 @@
 // console.log("sum of all values",sum);
 
 //assignment number 2//
-let no=[2,3,4,5,6,7,8,9,10,11,12,13,14,15];
-for(let i=no.length; i>0 ; i--){
+// let no=[2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+// for(let i=no.length; i>0 ; i--){
+//     console.log(no[i]);
+// }
+
+// assignment number 3
+
+let no=[1,2,33,44,5,6,7,8,88,210,887];
+let largeNo=no[0];
+for(let i=0; i<no.length; i++ )
+{
     console.log(no[i]);
+    if(no[i]> largeNo){
+        largeNo=no[i];
+    }
 }
+console.log("large no is :", largeNo);
