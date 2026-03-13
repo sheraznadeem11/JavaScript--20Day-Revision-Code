102e1a9fb6e4659f22e81da3677708b8b6cf7bb5
Sheraz Nadeem <sheraznadeem273@gmail.com>
2026-03-13 12:03:03 +0500

    array method indexof includes sort reverse join

--- Day 13/script.js
+++ Day 13/script.js
@@ -50,3 +50,30 @@
 // let newFruit=fruitName.splice(1,2,"Kevi","Barry");
 // console.log(fruitName)
 
+
+// Array indexof use  array input value find
+// let fruitName=["Apple","Banana","Orange","Dates","PineApple","Grapes","Watermellon"];
+// let newFruit=fruitName.indexOf("Banana");
+// console.log(newFruit);
+
+//Array Method includes 
+// let fruitName=["Apple","Banana","Orange","Dates","PineApple","Grapes","Watermellon"];
+// let allFruit=fruitName.includes("Orange");
+// console.log(allFruit);
+
+//Array method sort
+// let fruitName=["Banana","Apple","Orange","Dates","Charry","Grapes","Watermellon"];
+// console.log(fruitName)
+// let newFruit=fruitName.sort();
+// console.log(newFruit);
+
+//Array method reverse
+// let fruitName=["Apple","Banana","Orange","Dates","PineApple","Grapes","Watermellon"];
+// console.log(fruitName);
+// let allFruit=fruitName.reverse();
+// console.log(allFruit);
+
+//Array method join
+// let fruitName=["Apple","Banana","Orange","Dates","PineApple","Grapes","Watermellon"];
+// let jon=fruitName.join(":");
+// console.log(jon)
