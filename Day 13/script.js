2e4f73161e32ba7a912ad93f7007158ff3431800
Sheraz Nadeem <sheraznadeem273@gmail.com>
2026-03-13 09:41:41 +0500

    Array method all concept add or clear

---
+++ Day 13/script.js
@@ -0,0 +1,34 @@
+//Array method push
+// let fruitName=["Apple","Banana","Orange",];
+// fruitName.push("Charry","Dates");
+// console.log(fruitName);
+
+// Arry Method Pop
+// let fruitName=["Apple","Banana","Orange",];
+// fruitName.pop();
+// console.log(fruitName);
+
+
+// Array Method Shift
+// let fruitName=["Apple","Banana","Orange",];
+// fruitName.shift();
+// console.log(fruitName);
+
+// Arry Method Unshift
+// let fruitName=["Apple","Banana","Orange",];
+// let newFruit=fruitName.unshift("Charry","Dates");
+// console.log(fruitName);
+
+// Array Method Concat 
+// let fruitName=["Apple","Banana","Orange",];
+// let newFruit=["Cherry","WaterMellon"]
+// let allFruit=fruitName.concat(newFruit);
+// console.log(fruitName);
+// console.log(newFruit);
+// console.log(allFruit);
+
+// Array Method Slice 
+// let fruitName=["Apple","Banana","Orange","Cherry","Dates","PineApple","Burry","Beens"];
+// let sli=fruitName.slice(1,5);
+// console.log(fruitName);
+// console.log(sli)
