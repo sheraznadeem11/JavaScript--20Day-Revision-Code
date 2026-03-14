72dc31b3a8d66362e3c0a538de0660d05187df15
Sheraz Nadeem <sheraznadeem273@gmail.com>
2026-03-14 16:03:20 +0500

    Day 14 function concept

---
+++ Day 14/app.js
@@ -0,0 +1,12 @@
+//function 1 part
+// function data(a,b){
+//     console.log("Data Plue "+10+9+" !")
+// }
+// data();
+
+//function 2 part
+function value(c,b){
+    return c+b;
+}
+let data =value(25,20)
+console.log("Data Sum Show",data);
