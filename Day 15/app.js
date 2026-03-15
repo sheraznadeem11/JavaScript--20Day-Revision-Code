2eb8465755c5d9b2ed9f335a8d19d1e328e929f9
Sheraz Nadeem <sheraznadeem273@gmail.com>
2026-03-15 11:14:04 +0500

    Arrow function use  big value print or small value print or odd number print

---
+++ Day 15/app.js
@@ -0,0 +1,29 @@
+//Print big number use function or if else //
+// function value(num1,num2){
+//     if(num1 >num2){
+//         return num1;
+//     }else{
+//         return num2;
+//     }
+// }
+// let bignum=value(15,12);
+// console.log(bignum);
+
+//Print small number 
+//  function small(num1,num2){
+//     if(num1<num2){
+//         return num1;
+//     }else{
+//         return num2;
+//     }
+//  }
+//  let smallnum=small(5,10);
+//  console.log(smallnum);
+
+
+// Assignment number 2
+function oddNumber(x){
+    return x %2 === 0;
+}
+let odNUm=oddNumber(23);
+console.log("Is Number Even: ",odNUm);
