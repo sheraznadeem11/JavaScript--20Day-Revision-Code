073c0f45d18c8ac7af6092a6ec9411d5809489da
Sheraz Nadeem <sheraznadeem273@gmail.com>
2026-03-16 12:03:41 +0500

    Reduce method complete 2 quesion

--- Day 17 /script.js
+++ Day 17 /script.js
@@ -6,8 +6,26 @@
 // console.log(animalName);
 
 // question 2
-const number=[1,3,2,4,5,6,8,9,10,20,45,50,55,90,];
-const num=number.filter(val=>{
-    return val %2 ===0;
+// const number=[1,3,2,4,5,6,8,9,10,20,45,50,55,90,];
+// const num=number.filter(val=>{
+//     return val %2 ===0;
+// });
+// console.log(num);
+
+
+//   Reduce Method 
+// let number=[10,20,25,5,12,40,50];
+// let num=number.reduce((oldValue,newValue) => {
+//  return oldValue+newValue;
+// });
+// console.log(number);
+// console.log("Value Sum",num);
+
+
+// Reduct quesion 2
+
+let number=[10,20,25,12,40,50,100];
+let max=number.reduce((oldValue,newValue) => {
+ return newValue>oldValue?  newValue:oldValue;
 });
-console.log(num);
+console.log(max);
