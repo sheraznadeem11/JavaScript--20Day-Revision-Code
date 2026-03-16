0c0f5e24c298d85097b3b23ba6ac73e24b38656a
Sheraz Nadeem <sheraznadeem273@gmail.com>
2026-03-16 11:43:05 +0500

    Day 17 Filter concept complete 2 quesion

---
+++ Day 17 /script.js
@@ -0,0 +1,13 @@
+//  filter  question 1
+// const animal=["do","cat","bear","Wolf"];
+// const animalName=animal.filter(text=>{
+//     return text.length ==3
+// });
+// console.log(animalName);
+
+// question 2
+const number=[1,3,2,4,5,6,8,9,10,20,45,50,55,90,];
+const num=number.filter(val=>{
+    return val %2 ===0;
+});
+console.log(num);
