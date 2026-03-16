5a6e22c4b99f949a1abbae6deffd568fe1e01c48
Sheraz Nadeem <sheraznadeem273@gmail.com>
2026-03-16 11:13:09 +0500

    tomorrow code add

--- Day 16/script.js
+++ Day 16/script.js
@@ -1,10 +1,19 @@
-const People=[
-    {name:"Sheraz",age:20},
-    {name:"Monsaf",age:26},
-    {name:"Ali",age:30}
-];
-const allPeople=People.map(val=>{
-    return val.name;
-});
-console.log(People);
-console.log(allPeople);
+// const People=[
+//     {name:"Sheraz",age:20},
+//     {name:"Monsaf",age:26},
+//     {name:"Ali",age:30}
+// ];
+// const allPeople=People.map(val=>{
+//     return val.name;
+// });
+// console.log(People);
+// console.log(allPeople);
+
+
+                                // Mini Project Map
+let products=[
+    {name:"Fashwash", price:45000},
+    {name:"Makeup kit", price:30000},
+    {name:"Scrub", price:50000}
+]
+console.log(products);
