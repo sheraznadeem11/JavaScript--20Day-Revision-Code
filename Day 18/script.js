394103e996e718da87d8d7d34d4af855eba8541b
Sheraz Nadeem <sheraznadeem273@gmail.com>
2026-03-17 16:15:33 +0500

    Dom Manipulation

--- Day 18/script.js
+++ Day 18/script.js
@@ -1,9 +1,22 @@
+        // js use html change text or style
 // // console.log(document.body);
 // console.dir(document.body.childNodes[1]);
 // console.dir(document.body.childNodes[3]);
 // // document.body.childNodes[3].childNodes[3].innerText="DOM Assignment with shahid naeem";
 // document.getElementsByTagName("h3")[0].innerText = "DOM Assignment with sheraz Nadeem";
 
-document.querySelector("#myid").innerText="java script for sheraz nadeem";
-document.querySelector("h3").style.color="Ghostwhite";
-document.querySelector("button").innerText="Submit";
+                // use document.querySelector and change inner text color change 
+// document.querySelector("#myid").innerText="java script for sheraz nadeem";
+// document.querySelector("h3").style.color="Ghostwhite";
+// document.querySelector("button").innerText="Submit"; 
+
+
+    //    DOM Manipulation
+let val=document.querySelector("div");
+console.log(val.tagName);
+console.log(val.innerText);
+console.log(val.innerHTML);
+let data=document.querySelector("h3");
+console.log(data.firstChild)
+console.log(val.firstElementChild)
+
