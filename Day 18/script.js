3bdc4595a28ef6257c9f909dbde0071720ac0a6b
Sheraz Nadeem <sheraznadeem273@gmail.com>
2026-03-17 09:43:43 +0500

    Assignment complete QUesion DOM Document Object model

---
+++ Day 18/script.js
@@ -0,0 +1,5 @@
+// console.log(document.body);
+console.dir(document.body.childNodes[1]);
+console.dir(document.body.childNodes[3]);
+// document.body.childNodes[3].childNodes[3].innerText="DOM Assignment with shahid naeem";
+document.getElementsByTagName("h3")[0].innerText = "DOM Assignment with Shahid Nadeem";
