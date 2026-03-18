8714f0157ac2f188e768432f2e73e85d4eb5dc5b
Sheraz Nadeem <sheraznadeem273@gmail.com>
2026-03-18 08:56:56 +0500

    Get or set attribute

--- Assignment day 18/app.js
+++ Assignment day 18/app.js
@@ -1,3 +1,15 @@
-let newtext=prompt("Enter New text ");
-let h1=document.querySelector("h1");
-h1.innerText=h1.innerText+newtext;
+// let newtext=prompt("Enter New text ");
+// let h1=document.querySelector("h1");
+// h1.innerText=h1.innerText+newtext;
+
+
+
+// color change  attribute access js to html file or tag
+//  get or set attribute 
+let para=document.querySelector("p");
+console.log(para.getAttribute("class"));
+console.log(para.setAttribute("class","mynewclass"));
+
+
+
+
