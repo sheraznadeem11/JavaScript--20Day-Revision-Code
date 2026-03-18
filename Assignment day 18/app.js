6a1f7b8310ea819762221df3c0ccf0db59483bdc
Sheraz Nadeem <sheraznadeem273@gmail.com>
2026-03-18 09:33:16 +0500

    create button use createElement or create tag or other style change html tag color create java script use

--- Assignment day 18/app.js
+++ Assignment day 18/app.js
@@ -14,15 +14,33 @@
 
 // style change use js 
 
-let heading=document.querySelector("h1");
-heading.style.color="purple";
+// let heading=document.querySelector("h1");
+// heading.style.color="purple";
 
 
-let p=document.querySelector("p");
-p.style.fontSize="16px";
+// let p=document.querySelector("p");
+// p.style.fontSize="16px";
 // p.style.visibility="hidden"
-let button=document.querySelector("button");
-button.style.background="orange";
+// let button=document.querySelector("button");
+// button.style.background="orange";
 
 
+ 
+           //create element 
+let create=document.createElement("h2");
+create.innerText="This is a new create Element";
+document.querySelector("div").append(create);
+create.style.color="gray";
+create.style.fontSize="20px";
 
+            // remove tag elemet//
+let h3=document.querySelector("h3");
+h3.remove();
+
+
+//   create button use create element before/
+let button=document.createElement("button");
+button.innerText="submit";
+document.querySelector("h2").after(button);
+button.style.background="green";
+button.style.color="white";
