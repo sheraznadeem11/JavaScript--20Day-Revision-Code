5b8e90a8d819370abb3c6fad9a2e8235a7fc97c2
Sheraz Nadeem <sheraznadeem273@gmail.com>
2026-03-18 09:02:02 +0500

    use java scirpt style change html tags or button

--- Assignment day 18/app.js
+++ Assignment day 18/app.js
@@ -6,10 +6,23 @@
 
 // color change  attribute access js to html file or tag
 //  get or set attribute 
-let para=document.querySelector("p");
-console.log(para.getAttribute("class"));
-console.log(para.setAttribute("class","mynewclass"));
+// let para=document.querySelector("p");
+// console.log(para.getAttribute("class"));
+// console.log(para.setAttribute("class","mynewclass"));
 
+
+
+// style change use js 
+
+let heading=document.querySelector("h1");
+heading.style.color="purple";
+
+
+let p=document.querySelector("p");
+p.style.fontSize="16px";
+// p.style.visibility="hidden"
+let button=document.querySelector("button");
+button.style.background="orange";
 
 
 
