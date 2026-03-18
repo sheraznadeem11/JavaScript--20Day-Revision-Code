b4ac59dd26371d89bad41d1bf54148b29a963868
Sheraz Nadeem <sheraznadeem273@gmail.com>
2026-03-18 11:21:06 +0500

    assignment solve to creat h1 js use or style color change fonsize change font style change

---
+++ Assignment 19/script.js
@@ -0,0 +1,12 @@
+// DOM Manipulation
+
+// Assignment #01
+
+let h1=document.createElement("h1");
+h1.innerText="Java Script";
+document.querySelector("body").append(h1);
+h1.style.color="blue";
+h1.style.fontSize="40px";
+h1.style.fontStyle="italic";
+
+
