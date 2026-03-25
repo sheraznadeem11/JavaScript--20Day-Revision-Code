31895cb17b088c6bbc5763026dec8bc61ad01f5b
Sheraz Nadeem <sheraznadeem273@gmail.com>
2026-03-25 15:35:49 +0500

    Callback funtion complete console print or display print

---
+++ CallBack /script.js
@@ -0,0 +1,11 @@
+nam=()=>{
+    console.log("My name is imran nadeem");
+}
+setTimeout(nam,5000);
+
+let div=document.querySelector("div");
+
+data=()=>{
+    div.innerText="My name is imran nadeem";
+}
+setTimeout(data,4000);
