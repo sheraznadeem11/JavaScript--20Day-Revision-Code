5137ffc9369e2ee1a573e395346e7f388e841135
Sheraz Nadeem <sheraznadeem273@gmail.com>
2026-03-27 16:46:10 +0500

    async await use promise

--- async await/script.js
+++ async await/script.js
@@ -1,5 +1,16 @@
-function myfunction(){
-    console.log("This is may function");
+function getResult(rollNo){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log("Result of Student : "+rollNo);
+            resolve("Successful" + rollNo)
+        },2000);
+    })
 }
 
-myfunction();
+async function result() {
+    await getResult(1);
+    await getResult(2);
+    await getResult(3);   
+}
+
+result();
